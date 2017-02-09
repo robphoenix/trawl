337203e79d7efc9cdad64bd74217927680e91623
Rob Phoenix <rob@robphoenix.com>
2017-02-09 16:06:03 +0000

    add Interface struct & New() func

--- main.go
+++ main.go
@@ -11,34 +11,29 @@ import (
 	"strings"
 )
 
+type Interface struct {
+	Name        string
+	IPv4Address string
+	IPv4Mask    string
+	IPv4Network string
+	IPv6Address string
+	MACAddress  string
+	MTU         int
+}
+
+type ExternalIP string
+
 func main() {
 
 	flag.Parse()
+	wantIfaces := flag.Args()
 
-	interfacesWant := flag.Args()
-
-	ifaces, err := net.Interfaces()
+	ifaces, err := getIfaces(wantIfaces)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var requestedIfaces []net.Interface
-
-	if len(interfacesWant) > 0 {
-		for _, ifw := range interfacesWant {
-			validIface, err := validIface(ifw, ifaces)
-			if err != nil {
-				log.Fatal(err)
-			}
-			requestedIfaces = append(requestedIfaces, validIface)
-		}
-	}
-
-	if len(interfacesWant) == 0 {
-		requestedIfaces = ifaces
-	}
-
-	for _, iface := range requestedIfaces {
+	for _, iface := range ifaces {
 		addrs, err := iface.Addrs()
 		if err != nil {
 			log.Fatal(err)
@@ -78,18 +73,18 @@ func main() {
 
 }
 
-func toDottedDec(cidr string) (string, error) {
-	integer, err := strconv.Atoi(cidr)
+func toDottedDec(cidr string) (s string, err error) {
+	maskBits := []string{"", "128", "192", "224", "240", "248", "252", "254", "255"}
+	n, err := strconv.Atoi(cidr)
 	if err != nil {
-		return "", err
+		return s, err
 	}
-	if integer > 32 || integer < 0 {
-		return "", fmt.Errorf("Not a valid network mask: %s", cidr)
+	if n > 32 || n < 0 {
+		return s, fmt.Errorf("Not a valid network mask: %s", cidr)
 	}
 
-	maskBits := []string{"", "128", "192", "224", "240", "248", "252", "254", "255"}
-	allOnes := integer / 8
-	someOnes := integer % 8
+	allOnes := n / 8
+	someOnes := n % 8
 	mask := make([]string, 4)
 
 	for i := 0; i < allOnes; i++ {
@@ -106,15 +101,78 @@ func toDottedDec(cidr string) (string, error) {
 		}
 	}
 
-	dottedDec := strings.Join(mask, ".")
-	return dottedDec, nil
+	return strings.Join(mask, "."), nil
 }
 
-func validIface(iface string, got []net.Interface) (ifg net.Interface, err error) {
+func checkIface(iface string, got []net.Interface) (ifg net.Interface, err error) {
 	for _, ifg := range got {
 		if iface == ifg.Name {
 			return ifg, nil
 		}
 	}
+	// TODO list available interfaces
 	return ifg, fmt.Errorf("sorry, interface [%s] is not available", iface)
+}
+
+func getIfaces(want []string) ([]net.Interface, error) {
+
+	gotIfaces, err := net.Interfaces()
+	if err != nil {
+		return []net.Interface{}, err
+		// log.Fatal(err)
+	}
+
+	if len(want) == 0 {
+		return gotIfaces, nil
+	}
+
+	var ifaces []net.Interface
+
+	// check interfaces are available
+	for _, wif := range want {
+		iface, err := checkIface(wif, gotIfaces)
+		// TODO allow valid interfaces even when given with invalid interfaces
+		if err != nil {
+			return []net.Interface{}, err
+			// log.Fatal(err)
+		}
+		ifaces = append(ifaces, iface)
+	}
+
+	return ifaces, nil
+}
+
+func New(iface net.Interface) *Interface {
+	addrs, err := iface.Addrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ipv4 := addrs[0].String()
+
+	ipv4Cidr := strings.Split(ipv4, "/")[1]
+	ipv4Mask, err := toDottedDec(ipv4Cidr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ip, ipnet, err := net.ParseCIDR(addrs[0].String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ipv6Address string
+	if len(addrs) > 1 {
+		ipv6Address = addrs[1].String()
+	}
+
+	return &Interface{
+		Name:        iface.Name,
+		IPv4Address: ip.String(),
+		IPv4Mask:    ipv4Mask,
+		IPv4Network: ipnet.String(),
+		IPv6Address: ipv6Address,
+		MTU:         iface.MTU,
+		MACAddress:  iface.HardwareAddr.String(),
+	}
 }
