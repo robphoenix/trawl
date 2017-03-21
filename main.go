e2f34ab054f00da9a36dd7b367eb85315a156965
Rob Phoenix <rob@robphoenix.com>
2017-03-21 14:50:26 +0000

    add trawl.go file

--- main.go
+++ main.go
@@ -5,29 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strconv"
-	"strings"
 
 	"github.com/rdegges/go-ipify"
 )
 
 const (
-	VERSION = "v0.1.0"
+	VERSION = "v0.1.1"
 )
-
-// Interface provides the information for a device interface
-type Interface struct {
-	Name        string
-	IPv4Address string
-	IPv4Mask    string
-	IPv4Network string
-	IPv6Address string
-}
 
 var version bool
 
 func init() {
-	// parse flags
 	flag.BoolVar(&version, "v", false, "print version and exit")
 	flag.Parse()
 }
@@ -68,96 +56,4 @@ func main() {
 	}
 
 	fmt.Printf("public      %s\n", string(pubIP))
-}
-
-// New instantiates an Interface object for the passed in net.Interface type
-// representing a device interface
-func New(iface net.Interface) (i *Interface, err error) {
-	addrs, err := iface.Addrs()
-	if err != nil {
-		return i, err
-	}
-
-	// linux & windows return ipv4 & ipv6 addresses in a different order
-	// so we can't rely on which will be where in the addrs array
-	ipv4, ipv6 := extractAddrs(addrs)
-
-	// get IPv4 address & dotted decimal mask
-	ipv4Split := strings.Split(ipv4, "/")
-	ipv4Address := ipv4Split[0]
-	ipv4Cidr := ipv4Split[1]
-	ipv4Mask, err := toDottedDec(ipv4Cidr)
-	if err != nil {
-		return i, err
-	}
-
-	// get IPv4 network
-	_, ipnet, err := net.ParseCIDR(addrs[0].String())
-	if err != nil {
-		return i, err
-	}
-
-	return &Interface{
-		Name:        iface.Name,
-		IPv4Address: ipv4Address,
-		IPv4Mask:    ipv4Mask,
-		IPv4Network: ipnet.String(),
-		IPv6Address: ipv6,
-	}, nil
-}
-
-func extractAddrs(addrs []net.Addr) (ipv4 string, ipv6 string) {
-	for _, addr := range addrs {
-		if a := addr.String(); strings.Contains(a, ":") {
-			ipv6 = a
-		}
-		if a := addr.String(); strings.Contains(a, ".") {
-			ipv4 = a
-		}
-	}
-	return
-}
-
-func (iface *Interface) String() string {
-	ifaceString := "%-10s  %-15s  %-15s  %-18s %s"
-	return fmt.Sprintf(
-		ifaceString,
-		iface.Name,
-		iface.IPv4Address,
-		iface.IPv4Mask,
-		iface.IPv4Network,
-		iface.IPv6Address,
-	)
-}
-
-func toDottedDec(cidr string) (s string, err error) {
-	maskBits := []string{"", "128", "192", "224", "240", "248", "252", "254", "255"}
-	n, err := strconv.Atoi(cidr)
-	if err != nil {
-		return s, err
-	}
-
-	if n > 32 || n < 0 {
-		return s, fmt.Errorf("Not a valid network mask: %s", cidr)
-	}
-
-	allOnes := n / 8
-	someOnes := n % 8
-	mask := make([]string, 4)
-
-	for i := 0; i < allOnes; i++ {
-		mask[i] = "255"
-	}
-
-	if maskBits[someOnes] != "" {
-		mask[allOnes] = maskBits[someOnes]
-	}
-
-	for i, octet := range mask {
-		if octet == "" {
-			mask[i] = "0"
-		}
-	}
-
-	return strings.Join(mask, "."), nil
 }
---
+++ trawl.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+// Interface provides the information for a device interface
+type Interface struct {
+	Name        string
+	IPv4Address string
+	IPv4Mask    string
+	IPv4Network string
+	IPv6Address string
+}
+
+// New instantiates an Interface object for the passed in net.Interface type
+// representing a device interface
+func New(iface net.Interface) (i *Interface, err error) {
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return i, err
+	}
+
+	// we can't rely on the order of the addresses in the addrs array
+	ipv4, ipv6 := extractAddrs(addrs)
+
+	// get IPv4 address & dotted decimal mask
+	ipv4Split := strings.Split(ipv4, "/")
+	ipv4Address := ipv4Split[0]
+	ipv4Cidr := ipv4Split[1]
+	ipv4Mask, err := toDottedDec(ipv4Cidr)
+	if err != nil {
+		return i, err
+	}
+
+	// get IPv4 network
+	_, ipnet, err := net.ParseCIDR(addrs[0].String())
+	if err != nil {
+		return i, err
+	}
+
+	return &Interface{
+		Name:        iface.Name,
+		IPv4Address: ipv4Address,
+		IPv4Mask:    ipv4Mask,
+		IPv4Network: ipnet.String(),
+		IPv6Address: ipv6,
+	}, nil
+}
+
+func extractAddrs(addrs []net.Addr) (ipv4 string, ipv6 string) {
+	for _, addr := range addrs {
+		if a := addr.String(); strings.Contains(a, ":") {
+			ipv6 = a
+		}
+		if a := addr.String(); strings.Contains(a, ".") {
+			ipv4 = a
+		}
+	}
+	return
+}
+
+func (iface *Interface) String() string {
+	ifaceString := "%-10s  %-15s  %-15s  %-18s %s"
+	return fmt.Sprintf(
+		ifaceString,
+		iface.Name,
+		iface.IPv4Address,
+		iface.IPv4Mask,
+		iface.IPv4Network,
+		iface.IPv6Address,
+	)
+}
+
+func toDottedDec(cidr string) (s string, err error) {
+	maskBits := []string{"", "128", "192", "224", "240", "248", "252", "254", "255"}
+	n, err := strconv.Atoi(cidr)
+	if err != nil {
+		return s, err
+	}
+
+	if n > 32 || n < 0 {
+		return s, fmt.Errorf("Not a valid network mask: %s", cidr)
+	}
+
+	allOnes := n / 8
+	someOnes := n % 8
+	mask := make([]string, 4)
+
+	for i := 0; i < allOnes; i++ {
+		mask[i] = "255"
+	}
+
+	if maskBits[someOnes] != "" {
+		mask[allOnes] = maskBits[someOnes]
+	}
+
+	for i, octet := range mask {
+		if octet == "" {
+			mask[i] = "0"
+		}
+	}
+
+	return strings.Join(mask, "."), nil
+}
